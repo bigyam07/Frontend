1691b31138eec6ae4e947f73cb540b39a24d85d1
b4u_UI <bigyamkarmacharya@gmail.com>
2025-02-03 23:29:57 +0545

    modified

--- todoList/todo.js
+++ todoList/todo.js
@@ -2,7 +2,7 @@ let inputBtn = document.getElementById("input-value");
 let addBtn = document.getElementById("add-button");
 let tasksBox = document.getElementById("tasks-box")
 let here = document.getElementById("here");
-
+let tasksBoxContainer = document.getElementById("tasks");
 addBtn.addEventListener('click', addTask);
 inputBtn.addEventListener('keypress', (e) => {
     if(e.key == "Enter") {
@@ -29,7 +29,10 @@ function addTask() {
     checkIcon.classList.add("fa-solid", "fa-check","hide");
 
     let Para = document.createElement("p");
+    // Para.setAttribute("id", "paragraph");
     Para.innerText = inputBtn.value;
+    Para.style.color = isDarkMode ? "white" : "#444";
+
 
     let taskRight = document.createElement("div");
     taskRight.classList.add("task-right",  "flex");
@@ -70,7 +73,7 @@ function addTask() {
 
     editBtn.addEventListener('click', () => {
         let inputField = document.createElement("input");
-        inputField.type = Text;
+        inputField.type = text;
         inputField.value = Para.innerText;
         inputField.classList.add("input-replace");
         taskLeft.replaceChild(inputField, Para);
@@ -89,5 +92,64 @@ function addTask() {
             }
         });
     });
+}
+
+let darkBtn = document.getElementById("dark-mode");
+let isDarkMode = false;
+darkBtn.addEventListener('click', darkMode);
+
+
+function darkMode() {
+    let container = document.querySelector(".container");
+    let box = document.querySelector(".box");
+    let labels = document.querySelectorAll("label");
+    let Para = document.querySelectorAll("p");
+
+    if(!isDarkMode) {
+        container.style.backgroundColor = "#282828";
+        box.style.backgroundColor = "#444";
+        box.style.boxShadow = "0 0 15px white";
+        Para.forEach((paragraph) => {
+            paragraph.style.color = "white";
+        });
+        
+        labels.forEach((label) => {
+            label.style.color = "white";
+        });
+        tasksBox.addEventListener("mouseover", (e) => {
+            if (e.target.closest(".tasks")) {
+                e.target.style.backgroundColor = "#8b8b8b";
+            }
+        });
+        
+        tasksBox.addEventListener("mouseout", (e) => {
+            if (e.target.closest(".tasks")) {
+                e.target.style.backgroundColor = "transparent";
+            }
+        });
+        isDarkMode = true;
+    } else {
+        container.style.backgroundColor = "white";
+        box.style.backgroundColor = "#F8F9FA";
+        box.style.boxShadow = "0px 0px 6px #b0b0b0";
+        labels.forEach((label) => {
+            label.style.color = "#6c757d";
+        });
+        Para.forEach((paragraph) => {
+            paragraph.style.color = "#444";
+        });
+        tasksBox.addEventListener("mouseover", (e) => {
+            if (e.target.classList.contains("tasks")) {
+                e.target.style.backgroundColor = "#eceef0";
+            }
+        });
+        
+        tasksBox.addEventListener("mouseout", (e) => {
+            if (e.target.classList.contains("tasks")) {
+                e.target.style.backgroundColor = "transparent";
+            }
+        });
+        isDarkMode = false;
+    }
 }
 
